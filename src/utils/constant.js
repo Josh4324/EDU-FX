f9f90e29730a3604aef789d8da9235638b216c92
Josh <adesanyajoshua@ymail.com>
2023-08-14 08:23:52 +0100

    update

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,3 +1,3 @@
 //export const http = "http://localhost:1000";
-export const http = "http://34.217.32.128";
+export const http = "https://app.stacademy.io";
 export const front = "http://localhost:3004";
